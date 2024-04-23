417f850f01ee4f0b391390c9f06b163df1128ac6
Ubuntu <Snodgrass@DemoApp.ti13kmjblswufhyw442phqoloe.px.internal.cloudapp.net>
2024-04-23 00:13:00 +0000

    Initial commit, our creds. have been entered

---
+++ source/nginx/front_page/src/api/http/config.ts
@@ -0,0 +1,36 @@
+export const CURRENT_CONFIG = {
+
+  // license
+  appId: '145515', // You need to go to the development website to apply.
+  appKey: 'ee165d7001aa8b6d097be9db950a7ed', // You need to go to the development website to apply.
+  appLicense: 'VfP69ATgWJFdNMjvOdxhi4lPOuBvVmN3ZNCpgFSeGW6gGKu/EAmDrT1hUK80TcHJRtFGXwT3ReAFeVaJEyIO6megqVy7RpRk+s6VmjbazMVc0Z7gcBr2EcDVSFDxbJgJKMZeXE851I+qClGgzCv8faQ1wH+iXoL3hVlL0W7Sezw=', // You need to go to the development website to apply.
+
+  // http
+  baseURL: '40.82.215.210:443/', // This url must end with "/". Example: 'http://192.168.1.1:6789/'
+  websocketURL: 'ws:40.82.215.210:443/api/v1/ws', // Example: 'ws://192.168.1.1:6789/api/v1/ws'
+
+  // livestreamingst
+  // RTMP  Note: This IP is the address of the streaming server. If you want to see livestream on web page, you need to convert the RTMP stream to WebRTC stream.
+  rtmpURL: 'Please enter the rtmp access address.', // Example: 'rtmp://192.168.1.1/live/'
+  // GB28181 Note:If you don't know what these parameters mean, you can go to Pilot2 and select the GB28181 page in the cloud platform. Where the parameters same as these parameters.
+  gbServerIp: 'Please enter the server ip.',
+  gbServerPort: 'Please enter the server port.',
+  gbServerId: 'Please enter the server id.',
+  gbAgentId: 'Please enter the agent id',
+  gbPassword: 'Please enter the agent password',
+  gbAgentPort: 'Please enter the local port.',
+  gbAgentChannel: 'Please enter the channel.',
+  // RTSP
+  rtspUserName: 'Please enter the username.',
+  rtspPassword: 'Please enter the password.',
+  rtspPort: '8554',
+  // Agora
+  agoraAPPID: 'Please enter the agora app id.',
+  agoraToken: 'Please enter the agora temporary token.',
+  agoraChannel: 'Please enter the agora channel.',
+
+  // map
+  // You can apply on the AMap website.
+  amapKey: 'Please enter the amap key.',
+
+}
